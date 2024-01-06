4add480030076b4b07d4ac67952f212fa13b6bab
Rares <rjianu@abcam.com>
2024-01-06 13:32:15 +0200

    feat: Utils modules

---
+++ utils/go.mod
@@ -0,0 +1,3 @@
+module github.com/Serares/undertown_v3/utils
+
+go 1.21.4
---
+++ utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ReplyError(w http.ResponseWriter, r *http.Request, status int, message string) {
+	http.Error(w, message, status)
+}
+
+func ReplySuccess(w http.ResponseWriter, r *http.Request, status int, message interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(message)
+	if err != nil {
+
+		w.WriteHeader(500)
+		return fmt.Errorf("error marshalling JSON: %s", err)
+	}
+	w.WriteHeader(status)
+	w.Write(dat)
+	return nil
+}
