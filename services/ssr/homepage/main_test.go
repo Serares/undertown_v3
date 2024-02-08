7d2661c1aba6c94a80d397066606c073991d82b7
Rares <rjianu@abcam.com>
2024-02-08 13:58:46 +0200

    chore: Add debugging test for homepage mux

---
+++ services/ssr/homepage/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+
+	"github.com/Serares/ssr/homepage/handlers"
+	"github.com/Serares/ssr/homepage/service"
+	"github.com/joho/godotenv"
+)
+
+func setupAPI(t *testing.T) (string, func()) {
+	t.Helper()
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		t.Error("error loading the .env file")
+	}
+
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	client := service.NewClient(log)
+	homeService := service.NewHomeService(log, client)
+	propertiesService := service.NewPropertiesService(log, client)
+	singlePropertyService := service.NewPropertyService(log, client)
+
+	m := http.NewServeMux()
+	propertiesHandler := handlers.NewPropertiesHandler(log, *propertiesService)
+	defaultHandler := handlers.NewDefaultHandler(log, homeService, singlePropertyService)
+
+	// This is not advised to use in prod
+	m.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("../assets"))))
+	m.Handle("/chirii/", propertiesHandler)
+	m.Handle("/vanzari/", propertiesHandler)
+	m.Handle("/", defaultHandler)
+
+	ts := httptest.NewServer(m)
+
+	return ts.URL, func() {
+		log.Info("Shutting down the test server")
+		ts.Close()
+	}
+}
+
+// This is an e2e test
+func TestPost(t *testing.T) {
+	url, cleanup := setupAPI(t)
+	fmt.Println("The server url", url)
+	// var wg sync.WaitGroup
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+	defer cleanup()
+}
