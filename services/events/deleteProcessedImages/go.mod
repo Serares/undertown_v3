12a8ad219418f66f83e301fe3d849ebd00a4d6c1
Rares <rjianu@abcam.com>
2024-02-14 09:58:37 +0200

    feat: Add the async lambdas

--- services/events/deleteProcessedImages/go.mod
+++ services/events/deleteProcessedImages/go.mod
@@ -1,21 +1,16 @@
-module github.com/Serares/undertown_v3/services/events/processImages
+module github.com/Serares/undertown_v3/services/events/deleteProcessedImages
 
 go 1.21.6
 
 require (
 	github.com/Serares/undertown_v3/utils v0.0.0
 	github.com/aws/aws-lambda-go v1.46.0
+	github.com/aws/aws-sdk-go-v2 v1.24.1
 	github.com/aws/aws-sdk-go-v2/config v1.26.6
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.48.1
 )
 
 require (
-	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-)
-
-require (
-	github.com/aws/aws-sdk-go-v2 v1.24.1 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.4 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.16 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
@@ -31,8 +26,9 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
-	github.com/chai2010/webp v1.1.1
 	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 )
 
 replace github.com/Serares/undertown_v3/utils => ../../../utils
---
+++ services/events/persistUpdateProperty/types/types.go
@@ -0,0 +1,49 @@
+package types
+
+const (
+	ErrorMethodNotSupported = "http method not supported"
+	ASSETS_BUCKET_ENV       = "ASSETS_BUCKET_NAME"
+)
+
+type POSTSuccessResponse struct {
+	PropertyId      string
+	HumanReadableId string
+}
+
+type RequestFeatures struct {
+	// Floor                            int64 This is not present in the UI template
+	EnergyClass                      string
+	EnergyConsumptionPrimary         string
+	EnergyEmissionsIndex             string
+	EnergyConsumptionGreen           string
+	DestinationResidential           bool
+	DestinationCommercial            bool
+	DestinationOffice                bool
+	DestinationHoliday               bool
+	OtherUtilitiesTerrance           bool
+	OtherUtilitiesServiceToilet      bool
+	OtherUtilitiesUndergroundStorage bool
+	OtherUtilitiesStorage            bool
+	FurnishedNot                     bool
+	FurnishedPartially               bool
+	FurnishedComplete                bool
+	FurnishedLuxury                  bool
+	InteriorNeedsRenovation          bool
+	InteriorHasRenovation            bool
+	InteriorGoodState                bool
+	HeatingTermoficare               bool
+	HeatingCentralHeating            bool
+	HeatingBuilding                  bool
+	HeatingStove                     bool
+	HeatingRadiator                  bool
+	HeatingOtherElectrical           bool
+	HeatingGasConvector              bool
+	HeatingInfraredPanels            bool
+	HeatingFloorHeating              bool
+}
+
+type POSTFormData struct {
+	Title  string
+	Images []string
+	Floor  int64
+}
