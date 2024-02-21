8e11c7676ddf2d7d2e94dcaa3bc139e05dc686a3
Rares <rjianu@abcam.com>
2024-02-21 19:10:09 +0200

    fix: Limit the concurrency of requests that the client starts

--- services/ssr/assets/js/custom/handle_presign.js
+++ services/ssr/assets/js/custom/handle_presign.js
@@ -22,6 +22,65 @@ function uploadFileToS3(presignedUrl, file) {
   });
 }
 
+// Function to chunk an array into batches
+function chunkArray(array, size) {
+  const chunkedArr = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunkedArr.push(array.slice(i, i + size));
+  }
+  return chunkedArr;
+}
+
+function processPresignBatch(filesBatch) {
+  const batchPromises = filesBatch.map((file) => {
+    return FetchPresign(file.name).then((presignedData) => ({
+      //
+      presignedData,
+      file,
+    }));
+  });
+  return Promise.all(batchPromises);
+}
+
+function processS3UploadBatch(presignDataFilesBatch) {
+  const batchPromises = presignDataFilesBatch.map(({ presignedData, file }) => {
+    let { presignedUrl, keyName } = presignedData;
+    return uploadFileToS3(presignedUrl, file).then(() => keyName);
+  });
+  return Promise.all(batchPromises);
+}
+
+// Function to process requests in batches
+function ProcessPresignInBatches(files, batchSize) {
+  const fileBatches = chunkArray(files, batchSize);
+  let promiseChain = Promise.resolve(); // Start with a resolved promise for chaining
+  const results = []; // Array to collect all results
+  const keyNames = [];
+
+  fileBatches.forEach((fileBatch) => {
+    promiseChain = promiseChain
+      .then(() => processPresignBatch(fileBatch))
+      .then((presignDataFilesResults) => {
+        console.log("Batch results:", presignDataFilesResults);
+        results.push(...presignDataFilesResults); // Collect results
+        // Optional: return a new promise if you want to wait between batches
+        return presignDataFilesResults;
+      })
+      .then((results) => {
+        return processS3UploadBatch(results);
+      })
+      .then((keyNamesResults) => {
+        keyNames.push(...keyNamesResults);
+        return new Promise((resolve) => setTimeout(resolve, 500));
+      })
+      .catch((err) => {
+        return err;
+      });
+  });
+
+  return promiseChain.then(() => keyNames);
+}
+
 // imageName: string
 // imagesFormKey: string
 // formElement: domElement
