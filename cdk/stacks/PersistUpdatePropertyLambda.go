d1c55b9002b2dccdb3631c788bfcd40bf59e99e6
Rares <rjianu@abcam.com>
2024-02-21 12:27:16 +0200

    feat: Infrastructure addition
    - Add PRIqueue to replace the raw-images-bucket events to processRawImageLambda
    
    - Add persistUpdatePropertyLambda grant to send messages to PRIqueue
    
    - Raw images bucket needs cors permissions for the domain that will handle the file uploads

--- cdk/stacks/PersistUpdatePropertyLambda.go
+++ cdk/stacks/PersistUpdatePropertyLambda.go
@@ -20,19 +20,20 @@ type PersistUpdatePropertyLambdaProps struct {
 	Env                        string
 	PUQueue                    awssqs.Queue
 	DeleteProcessedImagesQueue awssqs.Queue
+	PRIQueue                   awssqs.Queue
 }
 
 func PersistUpdatePropertyLambda(scope constructs.Construct, id string, props *PersistUpdatePropertyLambdaProps) awscdk.Stack {
 	stack := awscdk.NewStack(scope, &id, &props.StackProps)
 	var deleteProcessedImagesQueueUrl string
-	var PUQueueUrl string
-
-	if props.PUQueue.QueueUrl() != nil {
-		PUQueueUrl = *props.PUQueue.QueueUrl()
-	}
+	var processRawImagesQueueUrl string
 
 	if props.DeleteProcessedImagesQueue.QueueUrl() != nil {
 		deleteProcessedImagesQueueUrl = *props.DeleteProcessedImagesQueue.QueueUrl()
+	}
+
+	if props.PRIQueue.QueueUrl() != nil {
+		processRawImagesQueueUrl = *props.PRIQueue.QueueUrl()
 	}
 
 	persistUpdatePropertyEnv := map[string]*string{
@@ -41,12 +42,12 @@ func PersistUpdatePropertyLambda(scope constructs.Construct, id string, props *P
 		// don't store them in plain text
 		// store them as an encrypted string?
 		// how to decrypt them
-		env.DB_HOST:          jsii.String(os.Getenv(env.DB_HOST)),
-		env.DB_NAME:          jsii.String(os.Getenv(env.DB_NAME)),
-		env.DB_PROTOCOL:      jsii.String(os.Getenv(env.DB_PROTOCOL)),
-		env.TURSO_DB_TOKEN:   jsii.String(os.Getenv(env.TURSO_DB_TOKEN)),
-		env.SQS_PU_QUEUE_URL: jsii.String(PUQueueUrl),
+		env.DB_HOST:        jsii.String(os.Getenv(env.DB_HOST)),
+		env.DB_NAME:        jsii.String(os.Getenv(env.DB_NAME)),
+		env.DB_PROTOCOL:    jsii.String(os.Getenv(env.DB_PROTOCOL)),
+		env.TURSO_DB_TOKEN: jsii.String(os.Getenv(env.TURSO_DB_TOKEN)),
 		env.SQS_DELETE_PROCESSED_IMAGES_QUEUE_URL: jsii.String(deleteProcessedImagesQueueUrl), // used to dispatch the names of images that needs to be deleted
+		env.SQS_PROCESS_RAW_IMAGES_QUEUE_URL:      jsii.String(processRawImagesQueueUrl),
 	}
 
 	s3BucketAccessRole := utils.CreateLambdaBasicRole(stack, "s3fullaccesslambdarole", props.Env)
@@ -73,12 +74,17 @@ func PersistUpdatePropertyLambda(scope constructs.Construct, id string, props *P
 			&awslambdaeventsources.SqsEventSourceProps{},
 		),
 	)
+
+	// SQS grants
 	// grant PUqueue access to addPropertyLambda
 	props.PUQueue.GrantConsumeMessages(persistUpdateProperty)
 
 	// ❗
 	// dispatch delete permissions
 	props.DeleteProcessedImagesQueue.GrantSendMessages(persistUpdateProperty)
+
+	// send process raw images message
+	props.PRIQueue.GrantSendMessages(persistUpdateProperty)
 
 	return stack
 }
